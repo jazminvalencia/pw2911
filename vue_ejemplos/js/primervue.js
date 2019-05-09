dab857ffc68e6ff8b7ef6a0d6a1f0e578301b010
Centro de Computo Dispositivos Moviles <ccdmadm@iMac-de-Centro-3.local>
2019-05-09 11:53:21 -0600

    primer vue terminado

--- vue_ejemplos/js/primervue.js
+++ vue_ejemplos/js/primervue.js
@@ -8,6 +8,30 @@ const app = new Vue({
 			{nombre: 'Peras',cantidad: 0},
 			{nombre: 'Mango',cantidad: 20},
 			{nombre: 'Platano',cantidad: 80},
-		]
+		],
+		nuevaFruta: '',
+		total: 0
+	},
+	methods:{
+		agregarFruta(){
+			//console.log("diste clic");
+			if(this.nuevaFruta!=""){
+				this.frutas.push(this.nuevaFruta);
+				this.frutas2.push({
+					nombre :this.nuevaFruta,
+					cantidad:0
+				})
+				this.nuevaFruta = "";	
+			}
+		}
+	},
+	computed:{
+		sumarFrutas(){
+			this.total=0
+			for(fruta of this.frutas2){
+				this.total = this.total+fruta.cantidad;
+			}
+			return this.total;
+		}
 	}
 });
